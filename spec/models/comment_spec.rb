26d18a7dc8997c5f279f8e9a3db32e6baea8fa85
heybluguy <heybluguy@yahoo.com>
2018-02-05 22:25:00 -0700

    Add validation for present attributes for comment.

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -4,9 +4,19 @@ describe Comment do
   describe "validations" do
     context "invalid attributes" do
       it "is invalid without content" do
-        comment = Comment.new()
+        job = Job.new(level_of_interest: 80, description: "Wahoo", city: "Denver")
+        comment = Comment.new(job: job)
 
         expect(comment).to be_invalid
+      end
+    end
+
+    context "valid attibutes" do
+      it "is valid with content" do
+        job = Job.new(level_of_interest: 80, description: "Wahoo", city: "Denver")
+        comment = Comment.new(content: "Great Place.", job: job)
+
+        expect(comment).to be_valid
       end
     end
   end
